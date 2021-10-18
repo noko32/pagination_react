4b72e09f9bf9edf26204c742e3defa1ee197550a
noko32 <pablocesararmenta@gmail.com>
2021-10-18 11:45:36 -0500

    Added Posts and pagination components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Posts from './components/Posts'
+import Paginations from './components/Paginations'
 import './App.css';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage, setPostPerPage] = useState(10);
+  const [postsPerPage, setPostPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,16 +16,24 @@ const App = () => {
       const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
       setPosts(res.data);
       setLoading(false);
+
     }
+
     fetchPosts();
   }, []);
 
+  // get current post
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="container mt-5">
+      <h1 className="text-light mb-3">My blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Paginations postsPerPage={postsPerPage} totalPosts={posts.length} />
     </div>
   );
-}
+};
 
 export default App;
